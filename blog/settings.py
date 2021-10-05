f6c23a45d9f50a1c8fcca0144a255e3d692c5709
km <charlieneckson@gmail.com>
2021-10-05 20:21:52 +0300

    Pretty templates

--- blog/settings.py
+++ blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "messel",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,9 @@ LOGIN_REDIRECT_URL = '/'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
